e6a5bcfc064ba289e3ad5d800933eb99cff0b192
LaxminarayanaV7416 <laxminarayana.vadnala@gmail.com>
2024-10-11 11:25:52 -0500

    added Navbar and flowbite css component to the website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './styles/images/logo.svg';
 import './styles/css/App.css';
+import { NavBarComponent } from './components/navbarComponent';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBarComponent/>
     </div>
   );
 }
